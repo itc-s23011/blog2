a117664011836869f44e628c12d14003a6ea9c58
itc-s23011 <s23011@std.it-college.ac.jp>
2024-02-02 02:48:08 +0000

    ch6.5

--- components/meta.js
+++ components/meta.js
@@ -12,7 +12,10 @@ const {
   siteIcon
 } = siteMeta
 
-const Meta = ({ pageTitle, pageDesc }) => {
+// OGP
+import siteImg from 'images/ogp.jpg'
+
+const Meta = ({ pageTitle, pageDesc, pageImg, pageImgW, pageImgH }) => {
   // ページタイトル
   const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
 
@@ -22,6 +25,12 @@ const Meta = ({ pageTitle, pageDesc }) => {
   // ページのURL
   const router = useRouter()
   const url = `${siteUrl}${router.asPath}`
+
+  // OGP画像
+  const img = pageImg || siteImg.src
+  const imgW = pageImgW || siteImg.width
+  const imgH = pageImgH || siteImg.height
+  const imgUrl = img.startsWith('https') ? img : `${siteUrl}${img}`
 
   return (
     <Head>
@@ -40,6 +49,11 @@ const Meta = ({ pageTitle, pageDesc }) => {
 
       <link rel='icon' href={siteIcon} />
       <link rel='apple-touch-icon' href={siteIcon} />
+
+      <meta property='og:image' content={imgUrl} />
+      <meta property='og:image:width' content={imgW} />
+      <meta property='og:image:height' content={imgH} />
+      <meta name='twitter:card' content='summary_large_image' />
     </Head>
   )
 }
