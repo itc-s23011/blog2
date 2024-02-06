b133be0b4e2ae45f6ef3779ad9cc9d101654782c
itc-s23011 <s23011@std.it-college.ac.jp>
2024-02-06 07:59:05 +0000

    ch7.4 p212まで

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,21 +1,12 @@
-import { client } from 'lib/api'
-
 const Schedule = () => {
   return <h1>記事のタイトル</h1>
 }
 export default Schedule
 
-export async function getStaticProps {
-  const rePromise = client.get({
-    endpoint: 'blogs',
-  })
-
-  try{
-    const res = await resPromise
-    console.log(res)
-  } catch (err) {
-    console.log(err)
-  }
+export async function getStaticProps () {
+  console.log('処理1')
+  setTimeout(() => console.log('処理2'), 1000)
+  console.log('処理3')
 
   return {
     props: {}
